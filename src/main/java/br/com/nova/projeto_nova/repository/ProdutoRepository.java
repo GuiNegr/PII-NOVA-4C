e55cc1a4b3950638b2f25de2080e46098ce24daa
Kauan Feitosa Mendes <kmendes@dm-pluris.plurismidia.com.br>
2024-09-18 18:07:06 -0300

    Listagem de produtos

--- src/main/java/br/com/nova/projeto_nova/repository/ProdutoRepository.java
+++ src/main/java/br/com/nova/projeto_nova/repository/ProdutoRepository.java
@@ -4,10 +4,18 @@ import br.com.nova.projeto_nova.bean.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+<<<<<<< HEAD
 import java.util.Optional;
+=======
+import java.util.List;
+>>>>>>> b337eca (Listagem de produtos)
 
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+<<<<<<< HEAD
     Optional<Produto> findBynomeProduto(String nomeProd);
+=======
+    List<Produto> findAllByOrderByIdProdutoDesc();
+>>>>>>> b337eca (Listagem de produtos)
 }
