46b67b4d0171a8c9876fe37068d267937649967b
KauanFM <kauanmendes.5@hotmail.com>
2024-11-08 13:29:31 -0300

    Modificando PedidoRequestDTO

--- src/main/java/br/com/nova/projeto_nova/bean/dto/PedidoRequestDTO.java
+++ src/main/java/br/com/nova/projeto_nova/bean/dto/PedidoRequestDTO.java
@@ -1,14 +1,10 @@
 package br.com.nova.projeto_nova.bean.dto;
-
-import br.com.nova.projeto_nova.bean.entity.Produto;
 import br.com.nova.projeto_nova.bean.entity.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
